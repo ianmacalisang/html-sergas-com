9c0cb585522fdc6e152e0096544d414ef6d0251d
ian andrew macalisang <andyware@live.com>
2023-03-06 13:19:23 +0800

    major update

---
+++ js/functions.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  setTimeout(function () {
+    $(".gallery-carousel").slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+    $(".gallery-carousel .image img").click(function () {
+      var datasrc = $(this).attr("src");
+      console.log(datasrc);
+      $("#image-viewer").modal("show");
+      $("#image-viewer .modal-content img").attr("src", datasrc);
+    });
+  }, 500);
+});
--- js/loader.js
+++ js/loader.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       setTimeout(function () {
         $(".load-submenu").load("./views/inner/home/nav-menu.html");
       }, 500);
-    } else if (URI == "/business") {
+    } else if (URI == "/business/") {
       $("title").html("For Business | Sergas Group of Companies");
       $("#viewsContainer").load("./views/business.html");
       setTimeout(function () {
@@ -22,7 +22,7 @@ $(document).ready(function () {
     }
 
     // start loading all about inner pages
-    else if (URI == "/about/message-from-management") {
+    else if (URI == "/about/message-from-management/") {
       $("title").html(
         "About Us | Message From Management | Sergas Group of Companies"
       );
@@ -30,13 +30,20 @@ $(document).ready(function () {
       setTimeout(function () {
         $(".load-submenu").load("./views/inner/about/nav-menu.html");
       }, 500);
-    } else if (URI == "/about/history") {
+    } else if (URI == "/about/why-choose-lpg/") {
+      $("title").html("About Us | Choose LPG | Sergas Group of Companies");
+      $("#viewsContainer").load("./views/inner/about/about-us.html");
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-loader").load("./partials/sectors.html");
+      }, 500);
+    } else if (URI == "/about/history/") {
       $("title").html("About Us | Our History | Sergas Group of Companies");
       $("#viewsContainer").load("./views/inner/about/history.html");
       setTimeout(function () {
         $(".load-submenu").load("./views/inner/about/nav-menu.html");
       }, 500);
-    } else if (URI == "/about/sergas-group-members") {
+    } else if (URI == "/about/sergas-group-members/") {
       $("title").html(
         "About Us | Our Group Members | Sergas Group of Companies"
       );
@@ -44,7 +51,7 @@ $(document).ready(function () {
       setTimeout(function () {
         $(".load-submenu").load("./views/inner/about/nav-menu.html");
       }, 500);
-    } else if (URI == "/about/qhse-policy") {
+    } else if (URI == "/about/qhse-policy/") {
       $("title").html("About Us | QHSE Policy | Sergas Group of Companies");
       $("#viewsContainer").load("./views/inner/about/policy.html");
       setTimeout(function () {
@@ -52,13 +59,124 @@ $(document).ready(function () {
       }, 500);
     }
     // end loading all about inner pages
-    else if (URI == "/contact") {
-      $("title").html("Contact Us | Sergas Group of Companies");
+
+    // start loading all about nav-links
+    else if (URI == "/about/news-and-media/") {
+      $("title").html("About Us | News and Media | Sergas Group of Companies");
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/news-and-media.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/agriculture/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Agriculture | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/agriculture.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/hotels/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Hotels | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/hotels.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/labor-camp/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Labor Camp | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/labor-camp.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/malls/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Malls | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/malls.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/restaurants/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Restaurants | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/restaurants.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    } else if (URI == "/about/sectors-we-serve/hospitals-and-labs/") {
+      $("title").html(
+        "About Us | Sectors We Serve | Hospitals and Labs | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/about/nav-links/sectors-we-serve/hospitals-and-labs.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/about/nav-menu.html");
+        $("#sector-form-loader").load("./partials/inquiry-form.html");
+        $("#business-cta").load("./partials/business-cta.html");
+        $("#image-viewer-loader").load("./partials/image-viewer.html");
+      }, 500);
+    }
+    // end loading all about nav-links
+
+    // start loading business page nav links
+
+    // end loading business page nav links
+
+    // start loading contact us links
+    else if (URI == "/contact-us/") {
+      $("title").html(
+        "Contact Us | Invest with Sergas | Sergas Group of Companies"
+      );
       $("#viewsContainer").load("./views/contact.html");
       setTimeout(function () {
         $(".load-submenu").load("./views/inner/contact/nav-menu.html");
       }, 500);
-    } else {
+    } else if (URI == "/contact-us/leave-a-feedback/") {
+      $("title").html(
+        "Contact Us | B2B: Leave a Feedback | Sergas Group of Companies"
+      );
+      $("#viewsContainer").load(
+        "./views/inner/contact/nav-links/leave-a-feedback.html"
+      );
+      setTimeout(function () {
+        $(".load-submenu").load("./views/inner/contact/nav-menu.html");
+      }, 500);
+    }
+    // end loading contact us links
+    else {
       alert("Page Not Found");
     }
   }
@@ -66,16 +184,27 @@ $(document).ready(function () {
     setTimeout(function () {
       if (URI == "/") {
         $(".homeLink").addClass("active");
-      } else if (URI == "/business") {
+      } else if (URI == "/business/") {
         $(".businessLink").addClass("active");
       } else if (
-        URI == "/about/sergas-group-members" ||
-        URI == "/about/message-from-management" ||
-        URI == "/about/history" ||
-        URI == "/about/qhse-policy"
+        URI == "/about/why-choose-lpg/" ||
+        URI == "/about/news-and-media/" ||
+        URI == "/about/sergas-group-members/" ||
+        URI == "/about/message-from-management/" ||
+        URI == "/about/history/" ||
+        URI == "/about/qhse-policy/" ||
+        URI == "/about/sectors-we-serve/agriculture/" ||
+        URI == "/about/sectors-we-serve/hotels/" ||
+        URI == "/about/sectors-we-serve/labor-camp/" ||
+        URI == "/about/sectors-we-serve/malls/" ||
+        URI == "/about/sectors-we-serve/restaurants/" ||
+        URI == "/about/sectors-we-serve/hospitals-and-labs/"
       ) {
         $(".aboutLink").addClass("active");
-      } else if (URI == "/contact") {
+      } else if (
+        URI == "/contact-us/" ||
+        URI == "/contact-us/leave-a-feedback/"
+      ) {
         $(".contactLink").addClass("active");
       }
     }, 300);
